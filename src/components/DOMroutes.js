0642158524167c73f8f0a1f00ee25f6cef629213
Barrett <Aliciawyse@users.noreply.github.com>
2018-01-24 13:05:16 -0500

    adding Lauren's react boilerplate to github

---
+++ src/components/DOMroutes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Domroutes = () =>
+  <ul className="nav nav-tabs">
+    <li className="home">
+      <Link to="/">Home</Link>
+    </li>
+    <li className="Login">
+      <Link to="/login">Login</Link>
+    </li>
+    <li className="frecipes">
+      <Link to="/find-recipes">Find Recipes</Link>
+    </li>
+    <li className="frest">
+      <Link to="/find-restaurants">Find Restaurants</Link>
+    </li>
+    <li
+      className="postrecipes">
+      <Link to="/post-recipes">Post Recipes</Link>
+    </li>
+    <li className="logout">
+      <Link to="/logout">Logout</Link>
+    </li>
+  </ul>;
+
+export default Domroutes;
