76a4695e5069ba1387ae40198a59f8142c8b3849
Lauren Spencer <laurenspencer@Laurens-MacBook-Pro.local>
2018-01-25 21:12:56 -0500

    WIP

--- client/src/components/pages/findrestaurant.js
+++ client/src/components/pages/findrestaurant.js
@@ -1,19 +1,39 @@
+import API from "../../api/API.js"
 import React, { Component } from "react";
 
-class Login extends Component {
+class Findres extends Component {
   // Setting the component's initial state
   state = {
     location: "",
+    results: []
   };
 
 
+handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
   handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
+    let query = this.state.location
     event.preventDefault();
     if (!this.state.location){
       alert("Please enter a city and state");
     } else {
-      alert(`Hello`);
+      console.log(query)
+      API.getRestaurants(query)
+      .then(res => {
+        console.log(res)
+        this.setState({ results: res.data })
+      })
+      .catch(err => console.log(err));
     }
 
     this.setState({
@@ -25,14 +45,12 @@ class Login extends Component {
     // Notice how each input has a `value`, `name`, and `onChange` prop
     return (
       <div>
-        <p>
-          Hello 
-        </p>
         <form className="form">
           <input
-            value={this.state.location}
             name="location"
+            value={this.state.location}
             type="text"
+            onChange={this.handleInputChange}
             placeholder="city, state"
           />
           <button onClick={this.handleFormSubmit}>Submit</button>
@@ -42,4 +60,7 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default Findres;
+
+
+ 
---
+++ db/routes/saverecipe.js
@@ -0,0 +1,33 @@
+
+var mongoose = require("mongoose");
+
+
+var axios = require("axios");
+
+var db = require("../models/addedreceipes.js");
+console.log(db)
+
+var array = [];
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/vegetarian_db", {
+  useMongoClient: true
+});
+
+// Routes
+module.exports = function (app){
+// A GET route for scraping the abcnews website
+
+  app.post("/postrecipes", function(req, res) {
+    console.log(req.body.recipe)
+    var comment = req.body.recipe
+    db.Recipes
+    .create(req.body.recipe)
+    .then(function(dbresult) {
+      console.log(dbresult)
+    })
+    })
+}
+  
