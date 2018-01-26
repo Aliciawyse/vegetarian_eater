8ce20a45bb447946258bde6cd3f9aa9e3b1b8c2b
Lauren Spencer <laurenspencer@Laurens-MacBook-Pro.local>
2018-01-25 20:33:23 -0500

    WIP post rec

--- vegetarian_eater/client/src/components/pages/postrecipe.js
+++ vegetarian_eater/client/src/components/pages/postrecipe.js
@@ -1,16 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Postrec = () =>
+class Postrec extends Component {
+  // Setting the component's initial state
+  state = {
+    title: "",
+    link: "",
+    ingredients: "",
+    instructions:""
+  };
+
+
+handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    let query = this.state.name
+    event.preventDefault();
+    if (!this.state.name){
+      alert("Please enter a name for this recipe");
+    } else {
+      console.log(query)
+
+    this.setState({
+    title: "",
+    link: "",
+    ingredients: "",
+    instructions:""
+    });
+
+  }
+  };
+
+render() {
+  return (
   <div>
     <h1>Post Recipes Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer
-      gravida dui mauris, ut interdum nunc egestas sed. Aenean sed mollis diam.
-      Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus tincidunt
-      fermentum. Suspendisse aliquet orci porta quam semper imperdiet. Praesent
-      euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
-  </div>;
-
+    <form className="form">
+          <input
+            name="title"
+            value={this.state.title}
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="name of recipe"
+          />
+          <input
+            name="link"
+            value={this.state.link}
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="link to recipe"
+          />
+          <input
+            name="ingredients"
+            value={this.state.ingredients}
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="Enter Ingredients"
+          />
+          <input
+            name="instructions"
+            value={this.state.instructions}
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="Enter Instructions"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+  </div>
+  )
+}
+}
 export default Postrec;
