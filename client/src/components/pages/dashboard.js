35a45d28ff417058af2edc1367e9ad18aa336dc8
Barrett <Aliciawyse@users.noreply.github.com>
2018-01-27 22:36:06 -0500

    updating dashboard pictures

--- client/src/components/pages/dashboard.js
+++ client/src/components/pages/dashboard.js
@@ -14,7 +14,7 @@ const Dashboard = ({match}) =>
         <Container style={{display: "flex", justifyContent: "space-evenly"}}>
             <Card style={{width:"30%"}} className="restaurantCard">
 
-                <Card.Image src='http://bulma.io/images/placeholders/1280x960.png' square='128by128'  />
+                <Card.Image src='https://i.pinimg.com/564x/dd/e9/07/dde9074f2c35ca171766d307b542823e.jpg' square='128by128'  />
                 <Card.Content>
                     <Content>
                         <Link to={`${match.url}/find-restaurants`}>Find Restaurants</Link>
@@ -25,7 +25,7 @@ const Dashboard = ({match}) =>
 
             <Card style={{width:"30%"}} className="restaurantCard">
 
-                <Card.Image src='http://bulma.io/images/placeholders/1280x960.png' square='128by128'  />
+                <Card.Image src='https://www.superhealthykids.com/wp-content/uploads/2008/10/refried-beans-24-768x1152.jpg' square='128by128'  />
                 <Card.Content>
                     <Content>
                         <Link to={`${match.url}/find-recipes`}>Find Recipes</Link>
@@ -36,7 +36,7 @@ const Dashboard = ({match}) =>
 
             <Card style={{width:"30%"}} className="restaurantCard">
 
-                <Card.Image src='http://bulma.io/images/placeholders/1280x960.png' square='128by128'  />
+                <Card.Image src='https://www.superhealthykids.com/wp-content/uploads/2016/09/stuffedpeppers-8-768x1152.jpg' square='128by128'  />
                 <Card.Content>
                     <Content>
                         <Link to={`${match.url}/post-recipes`}>Post Recipes</Link>
